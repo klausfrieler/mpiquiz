3efdfbe821ce7bf1fc2981b2f7e046f68d997ff1
klaus <kgf@omniversum.de>
2023-12-04 14:49:38 +0100

    Added composer quiz

--- data_raw/item_bank_generator.R
+++ data_raw/item_bank_generator.R
@@ -17,6 +17,13 @@ read_classical_item_bank <- function(){
   classical_item_bank
 }
 
+read_composer_item_bank <- function(){
+  composer_item_bank <- readxl::read_xlsx("data_raw/item_banks/classical_composer_item_bank.xlsx")
+  #SRS_item_bank[SRS_item_bank$correct == "none",]$correct <- "item5"
+  usethis::use_data(composer_item_bank, overwrite = TRUE)
+  composer_item_bank
+}
+
 read_jazz_item_bank <- function(){
   jazz_item_bank <- readxl::read_xlsx("data_raw/item_banks/jazz_item_bank.xlsx")
   #SRS_item_bank[SRS_item_bank$correct == "none",]$correct <- "item5"
@@ -40,12 +47,14 @@ read_pop_item_bank <- function(){
 
 metal_item_bank <- read_metal_item_bank()
 classical_item_bank <- read_classical_item_bank()
+composer_item_bank <- read_composer_item_bank()
 jazz_item_bank <- read_jazz_item_bank()
 hiphop_item_bank <- read_hiphop_item_bank()
 pop_item_bank <- read_pop_item_bank()
 
 MGQ_item_banks <- list("metal" = metal_item_bank,
                        "classical" = classical_item_bank,
+                       "classical_composer" = composer_item_bank,
                        "jazz" = jazz_item_bank,
                        "hiphop" = hiphop_item_bank,
                        "pop" = pop_item_bank)
