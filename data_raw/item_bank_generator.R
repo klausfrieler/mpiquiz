f8241af453a19258f5b776ce1139c55dbcfdea94
klaus <kgf@omniversum.de>
2023-12-06 21:50:43 +0100

    Changed metal quiz to power metal detection.

--- data_raw/item_bank_generator.R
+++ data_raw/item_bank_generator.R
@@ -3,8 +3,8 @@ library(tidyverse)
 
 read_metal_item_bank <- function(){
   metal_item_bank <- readxl::read_xlsx("data_raw/item_banks/metal_item_bank.xlsx")
-  metal_item_bank[metal_item_bank$role != "black_metal",]$role <- "target"
-  metal_item_bank[metal_item_bank$role == "black_metal",]$role <- "foil"
+  #metal_item_bank[metal_item_bank$role != "black_metal",]$role <- "target"
+  #metal_item_bank[metal_item_bank$role == "black_metal",]$role <- "foil"
   #SRS_item_bank[SRS_item_bank$correct == "none",]$correct <- "item5"
   usethis::use_data(metal_item_bank, overwrite = TRUE)
   metal_item_bank
